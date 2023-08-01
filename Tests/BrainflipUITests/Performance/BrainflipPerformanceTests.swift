e5237a23d07a977b3785c3f19416910abee72640
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-01 10:27:33 -0400

    add performance test for opening a document

--- Tests/BrainflipUITests/Performance/BrainflipPerformanceTests.swift
+++ Tests/BrainflipUITests/Performance/BrainflipPerformanceTests.swift
@@ -23,4 +23,29 @@ final class BrainflipPerformanceTests: XCTestCase {
             app.launch()
         }
     }
+    
+    /// Measures how long it takes to open a document.
+    func testOpenPerformance() throws {
+        let metrics: [XCTMetric] = [
+            XCTClockMetric(),
+            XCTCPUMetric(),
+            XCTMemoryMetric()
+        ]
+        let options = XCTMeasureOptions()
+        options.invocationOptions = .manuallyStart
+        
+        let programName = "Numwarp"
+        let menuBar = app.menuBars.firstMatch
+        let openSampleProgram = menuBar.menuItems["Sample Programs"].menuItems[programName]
+        let documentWindow = app.windows["\(programName).bf"]
+        
+        measure(metrics: metrics, options: options) {
+            app.launch()
+            
+            openSampleProgram.click()
+            
+            startMeasuring()
+            _ = documentWindow.waitForExistence(timeout: .infinity)
+        }
+    }
 }
