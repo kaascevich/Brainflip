258658c93e8e1ab0d1e160a0ff4d8e7fd819acfd
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-02 12:45:58 -0400

    remove unnecessary whitespace

--- Sources/Brainflip/Utilities/Language/InterpreterError.swift
+++ Sources/Brainflip/Utilities/Language/InterpreterError.swift
@@ -6,11 +6,11 @@ enum InterpreterError: String, Error {
     case mismatchedBrackets = "There are unmatched brackets within your code."
     
     /// Thrown when the pointer points to a cell that is below the bounds of the array.
-    case underflow          = "An attempt was made to go below the bounds of the array. \n\n(Hint: try raising the initial pointer location in the interpreter settings.)"
+    case underflow = "An attempt was made to go below the bounds of the array. \n\n(Hint: try raising the initial pointer location in the interpreter settings.)"
     
     /// Thrown when the pointer points to a cell that is above the bounds of the array.
-    case overflow           = "An attempt was made to go above the bounds of the array. \n\n(Hint: try increasing the array size or lowering the intiial pointer location in the interpreter settings.)"
+    case overflow = "An attempt was made to go above the bounds of the array. \n\n(Hint: try increasing the array size or lowering the intiial pointer location in the interpreter settings.)"
     
     /// Thrown when encountering a break instruction (if enabled).
-    case `break`            = ""
+    case `break` = ""
 }
