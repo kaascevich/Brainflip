5e3ce4b1dd6df14bc9f5a594a45343b5cbf47333
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-03 13:15:32 -0400

    remove confirmation text field

--- Sources/Brainflip/UI/Scenes/Settings/Views/General Settings/DefaultsButton.swift
+++ Sources/Brainflip/UI/Scenes/Settings/Views/General Settings/DefaultsButton.swift
@@ -4,26 +4,18 @@ struct DefaultsButton: View {
     @EnvironmentObject private var settings: AppSettings
     @State private var isWarningAboutReset = false
     
-    @State private var confirmationInput = ""
-    
-    private let appName = Bundle.main.object(forInfoDictionaryKey: kCFBundleNameKey as String) as! String
-
     var body: some View {
         Button("Reset All Settings…") {
             isWarningAboutReset.toggle()
         }
         .confirmationDialog("Are you sure you want to reset all settings to their defaults?", isPresented: $isWarningAboutReset) {
-            TextField("Type \"\(appName)\" to reset", text: $confirmationInput)
-            
             Button("Reset", role: .destructive) {
                 settings.resetAllToDefaults()
             }
-            .disabled(confirmationInput != appName)
             
             Button("Cancel", role: .cancel) { }
         } message: {
-            Text("Type \"\(appName)\" to confirm your intent. You cannot undo this action.")
-                .selectionDisabled()
+            Text("You cannot undo this action.")
         }
     }
 }
