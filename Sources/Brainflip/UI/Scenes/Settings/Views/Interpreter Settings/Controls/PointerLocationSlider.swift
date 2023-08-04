9e28beff572c2b847097bc8b68f66bcf6798addc
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-04 14:41:23 -0400

    replace NSNumber(floatLiteral:) with NSNumber(value:)

--- Sources/Brainflip/UI/Scenes/Settings/Views/Interpreter Settings/Controls/PointerLocationSlider.swift
+++ Sources/Brainflip/UI/Scenes/Settings/Views/Interpreter Settings/Controls/PointerLocationSlider.swift
@@ -5,7 +5,7 @@ struct PointerLocationSlider: View {
     var formatter: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.minimum = 0
-        formatter.maximum = NSNumber(floatLiteral: settings.arraySize - 1)
+        formatter.maximum = NSNumber(value: settings.arraySize - 1)
         return formatter
     }
     
