f8168f7943c6b0f81cd0dfabe274db6084be004f
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-04 15:21:03 -0400

    fix syntax highlighting not updating when settings change

--- Sources/Brainflip/UI/Scenes/Documents/Views/Main Window/Editor Pane/Editor/SyntaxHighlighting.swift
+++ Sources/Brainflip/UI/Scenes/Documents/Views/Main Window/Editor Pane/Editor/SyntaxHighlighting.swift
@@ -1,23 +1,30 @@
 import SwiftUI
 import HighlightedTextEditor
 
-struct SyntaxHighlighting {    
-    static var hashColor: Color = settings.breakOnHash ? .green : .gray
-    static var highlightPatterns: [(String, Color)] = [
-        ("[<>]",     .orange),   // "<>"
-        ("[+-]",     .red),      // "+-"
-        (#"[\[\]]"#, .brown),    // "[]"
-        ("[\\.,]",   .purple),   // ".,"
-        ("#",         hashColor) // "#"
-    ]
+// These properties all need to be computed in order to refresh the views.
+struct SyntaxHighlighting {
+    static var hashColor: Color {
+        settings.breakOnHash ? .green : .gray
+    }
+    static var highlightPatterns: [(String, Color)] {
+        [
+            ("[<>]",     .orange),   // "<>"
+            ("[+-]",     .red),      // "+-"
+            (#"[\[\]]"#, .brown),    // "[]"
+            ("[\\.,]",   .purple),   // ".,"
+            ("#",         hashColor) // "#"
+        ]
+    }
     
-    static var highlightRules: [HighlightRule] = highlightPatterns.map { (regex, color) in
-        HighlightRule(
-            pattern: try! NSRegularExpression(pattern: regex),
-            formattingRule: TextFormattingRule(
-                key: .foregroundColor,
-                value: NSColor(color)
+    static var highlightRules: [HighlightRule] {
+        highlightPatterns.map { (regex, color) in
+            HighlightRule(
+                pattern: try! NSRegularExpression(pattern: regex),
+                formattingRule: TextFormattingRule(
+                    key: .foregroundColor,
+                    value: NSColor(color)
+                )
             )
-        )
+        }
     }
 }
